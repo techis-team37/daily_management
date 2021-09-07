92b6b2d100d4c5c12d44e0b39161c3346204ad8b
nakamozu <mozu.mo16zu@gmail.com>
2021-09-07 19:50:16 +0900

    マイグレーションファイル追加

---
+++ database/migrations/2021_09_07_182851_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->string('product_name');
+            $table->string('image');
+            $table->string('tag');
+            $table->string('category');
+            $table->integer('stock');
+            
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
