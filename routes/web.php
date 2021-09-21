39c95a426fb6e32d53420ef296477515754f0a62
nakamozu <mozu.mo16zu@gmail.com>
2021-09-21 15:41:48 +0900

    個別ページ、編集画面、商品削除機能の実装

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use App\Http\Controllers\ProductController;
 */
 
 //Route::resource('/account',AccountController::class);
-//Route::resource('/product',ProductController::class);
-
 
 //Route::get('account/view',[AccountController::class,'create']);
 
@@ -25,6 +23,19 @@ Route::post('register',[AccountController::class,'register']);
 Route::post('login',[AccountController::class,'login']);
 //Route::view('mypage',[AccountController::class,'index']);
 
-//Route::get('/product',[ProductController::class, 'index']);
-//Route::get('/create',[ProductController::class, 'store']);
-
+Route::resource('/account',AccountController::class);
+// Route::resource('/product',ProductController::class);
+// 商品一覧画面を表示
+Route::get('/product/{account_id}',[ProductController::class, 'index']);
+// 商品登録画面を表示
+Route::get('/create',[ProductController::class, 'create']);
+// 商品登録の保存
+Route::post('/store',ProductController::class, 'store');
+// 商品個別ページの表示
+Route::get('/show/{product_id}',[ProductController::class, 'show']);
+// 商品編集画面の表示
+Route::get('/edit/{product_id}',[ProductController::class, 'edit']);
+// 商品編集の保存
+Route::PUT('/update',[ProductController::class, 'update']);
+// 登録商品の削除
+Route::get('/delete/{product_id}',[ProductController::class, 'destroy']);
