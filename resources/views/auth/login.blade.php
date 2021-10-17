01e0b4463b60ae5a4dc42d1467ff7422568dd54c
RUMItatsu <vete.gkmtk19850627@gmail.com>
2021-10-17 20:28:40 +0900

    会員登録時＆ログイン時エラー文表示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,6 +22,15 @@
 
 <form action="{{'/login'}}" method="POST" class="form">
 
+@if(count($errors) > 0)
+    <div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    </div>
+@endif
     {{csrf_field()}}
 
     <div class="form-body">
