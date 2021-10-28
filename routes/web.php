f48fe427a670211e6f53ca2fc9193e5759499b7d
sora0606 <shimadasora1406@gmail.com>
2021-10-28 14:33:37 +0900

    最初の画面の編集

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/', function(){return redirect('login');});
 
 //Route::resource('/account',AccountController::class);
 
